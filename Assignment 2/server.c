e210b890dfe634298a0ee9c4934ce952447e2a89
mahenaz-sayyed <61244534+mahenaz-sayyed@users.noreply.github.com>
2021-04-21 22:18:04 -0700

    Update server.c

--- Assignment 2/server.c
+++ Assignment 2/server.c
@@ -5,78 +5,89 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
-#include<errno.h>
+#include <pwd.h>
 #define PORT 8080
+
 int main(int argc, char const *argv[])
 {
-	int server_fd, new_socket, valread;
-	struct sockaddr_in address;
-	int opt = 1;
-	int addrlen = sizeof(address);
-	char *hello = "Hello from server";
-	pid_t curr_pid;
-	int priv=0;
-	int err;
-	
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    struct passwd *pwd;
+    pid_t curr_pid;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
+    char *user = "nobody";
 
-	printf("execve=0x%p\n", execve);
+    printf("execve=0x%p\n", execve);
 
-	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-	{
-	perror("socket failed");
-	exit(EXIT_FAILURE);
-	}
+    if (strcmp(argv[0], "c") == 0)
+    {
+        int new_socket_dup = atoi(argv[1]);
+        valread = read(new_socket_dup, buffer, 1024);
+        printf("%s\n", buffer);
+        send(new_socket_dup, hello, strlen(hello), 0);
+        printf("Hello message sent\n");
+        exit(0);
+    }
+    else
+    {
+        // Creating socket file descriptor
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+        {
+            perror("socket failed");
+            exit(EXIT_FAILURE);
+        }
 
-	// Forcefully attaching socket to the port 8080
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-		                                  &opt, sizeof(opt)))
-	{
-	perror("setsockopt");
-	exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons( PORT );
+        // Forcefully attaching socket to the port 8080
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                       &opt, sizeof(opt)))
+        {
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons(PORT);
 
-	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr *)&address,
-		                 sizeof(address))<0)
-	{
-	perror("bind failed");
-	exit(EXIT_FAILURE);
-	}
-	if (listen(server_fd, 3) < 0)
-	{
-	perror("listen");
-	exit(EXIT_FAILURE);
-	}
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-		       (socklen_t*)&addrlen))<0)
-	{
-	perror("accept");
-	exit(EXIT_FAILURE);
-	}
-	curr_pid=fork();
-	if(curr_pid==0){
-		printf("Running in the child proc\n");
-		int ret = execl("child", "child", &new_socket, hello, NULL);
-		printf("%d",&ret);
-		if (ret<0) {
-			printf("exec failed");
-			_exit(2);
-		}
-	}
-	else if(curr_pid>0){
-	
-		wait(2000);
-		printf("Returned to Parent...\n");
-
-	}
-	else{
-		perror("Unable to fork");
-		_exit(2);
-	}
-	
-	return 0;
+        // Forcefully attaching socket to the port 8080
+        if (bind(server_fd, (struct sockaddr *)&address,
+                 sizeof(address)) < 0)
+        {
+            perror("bind failed");
+            exit(EXIT_FAILURE);
+        }
+        if (listen(server_fd, 3) < 0)
+        {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                                 (socklen_t *)&addrlen)) < 0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        curr_pid = fork();
+        if (curr_pid == 0)
+        {
+            int cpy_parent_sock = dup(new_socket);
+            if (cpy_parent_sock == -1)
+            {
+                perror("dup did not work");
+            }
+            if ((pwd = getpwnam(user)) == NULL)
+            {
+                perror("Cannot find UID for nobody");
+            }
+            setuid(pwd->pw_uid);
+            char itoSt[10];
+            sprintf(itoSt, "%d", cpy_parent_sock);
+            char *args[] = {"c", itoSt, NULL};
+            execvp(argv[0], args);
+        }
+        wait();
+    }
+    return 0;
 }
